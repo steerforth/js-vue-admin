00690d12bc8ec6d7606b4baaecf0113230953c27
HP.steer <565355716@qq.com>
2020-08-25 19:36:07 +0800

    init

--- src/main.js
+++ src/main.js
@@ -7,11 +7,18 @@ import 'element-ui/lib/theme-chalk/index.css'
 
 import '@/styles/index.scss' // global css
 
-import App from './App.vue'
+import App from './App'
+import store from './store'
+import router from './router'
+
+import '@/icons' // icon
+import '@/permission' // permission control
 
 Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({
+  router,
+  store,
   render: h => h(App),
 }).$mount('#app')
